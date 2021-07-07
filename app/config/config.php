d3939e8b7b895c6eb879df9adc30fc3e63b51d27
IstvanFabian <fabian@aut.bme.hu>
2021-07-07 18:11:53 +0200

    new db name

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
 define('DB_USER', 'b12c2dff882973');
 define('DB_PASS', 'ffcd7dcf');
-define('DB_NAME', 'cryptomvcdb');
+define('DB_NAME', 'heroku_972dc9a1451dfb6');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'cryptomvc.herokuapp.com');

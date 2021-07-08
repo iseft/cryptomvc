03ba0fe9b138fa9f827dfcd9529e59536f7f2c64
IstvanFabian <fabian@aut.bme.hu>
2021-07-08 20:32:28 +0200

    https css

--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITENAME; ?></title>
-    <link rel="stylesheet" href="http://<?php echo URLROOT ?>/public/css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://<?php echo URLROOT ?>/public/css/style.css" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
